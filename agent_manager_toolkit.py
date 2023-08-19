ac2cfd1130244de5950699a455755c50942abe0a
Andrew Kelly <andrew.kelly@neutralaiz.com>
2023-08-19 11:23:08 -0400

    omg i think I fixed it!

--- agent_manager_toolkit.py
+++ agent_manager_toolkit.py
@@ -1,8 +1,8 @@
 from abc import ABC
 from typing import List
 from superagi.tools.base_tool import BaseTool, BaseToolkit
-from agent_manager_list_agent import ListAgentInput
-from agent_manager_current_agent import CurrentAgentInput
+from agent_manager_list_agent import ListAgentTool
+from agent_manager_current_agent import CurrentAgentTool
 
 class AgentManagerToolkit(BaseToolkit, ABC):
     name: str = "AgentManager Toolkit"
@@ -10,7 +10,7 @@ class AgentManagerToolkit(BaseToolkit, ABC):
 
     def get_tools(self) -> List[BaseTool]:
         return [
-            ListAgentInput(), CurrentAgentInput(),
+            ListAgentTool(), CurrentAgentTool(),
         ]
 
     def get_env_keys(self) -> List[str]:
