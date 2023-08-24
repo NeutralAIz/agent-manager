4877c3d677f7008945441fb400c16f384ea4101a
Andrew Kelly <andrew.kelly@neutralaiz.com>
2023-08-24 12:53:42 -0400

    New tool for agent execution

--- agent_manager_toolkit.py
+++ agent_manager_toolkit.py
@@ -3,6 +3,7 @@ from typing import List
 from superagi.tools.base_tool import BaseTool, BaseToolkit
 from agent_manager_list_agent import ListAgentTool
 from agent_manager_current_agent import CurrentAgentTool
+from agent_manager_new_run_agent import NewRunAgentTool
 
 class AgentManagerToolkit(BaseToolkit, ABC):
     name: str = "AgentManager Toolkit"
@@ -10,7 +11,7 @@ class AgentManagerToolkit(BaseToolkit, ABC):
 
     def get_tools(self) -> List[BaseTool]:
         return [
-            ListAgentTool(), CurrentAgentTool(),
+            ListAgentTool(), CurrentAgentTool(), NewRunAgentTool()
         ]
 
     def get_env_keys(self) -> List[str]:
