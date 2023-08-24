6cea152ca051c50644f71e998876c94659796dc9
Andrew Kelly <andrew.kelly@neutralaiz.com>
2023-08-23 21:03:58 -0400

    Fixes

--- agent_manager_list_agent.py
+++ agent_manager_list_agent.py
@@ -7,6 +7,12 @@ from superagi.models.toolkit import Toolkit
 from superagi.models.agent import Agent
 from superagi.models.project import Project
 from superagi.models.organisation import Organisation
+import decimal
+from datetime import date, datetime, time, timedelta
+from collections.abc import Iterable
+from sqlalchemy.orm.collections import InstrumentedList
+from uuid import UUID
+from enum import Enum
 
 
 class ListAgentInput(BaseModel):
@@ -35,10 +41,38 @@ class ListAgentTool(BaseTool):
             JSON representation of all the agents from default project
         """
     
-        def object_as_dict(obj):
-            return {c.key: getattr(obj, c.key)
-                for c in inspect(obj).mapper.column_attrs}
-
+        def json_serial(obj):
+            if isinstance(obj, (datetime, date, time)):
+                return obj.isoformat()
+            elif isinstance(obj, timedelta):
+                return str(obj)
+            elif inspect(obj).is_mapper:  # SQLAlchemy check
+                return {key: serialize(val) for key, val in obj.__dict__.items() if not key.startswith('_')} 
+            elif isinstance(obj, UUID):  # Handle UUID objects.
+                return str(obj)
+            elif isinstance(obj, decimal.Decimal):  # Handle Decimal objects.
+                return float(obj)
+            elif isinstance(obj, Enum):  # Handle Enum objects.
+                return obj.name
+            elif isinstance(obj, InstrumentedList):  # Handle SQLAlchemy relationship objects.
+                return [serialize(elem) for elem in obj]
+            elif isinstance(obj, Iterable):
+                return [serialize(item) for item in obj]
+            raise TypeError("Type %s not serializable" % type(obj)) 
+        
+        def serialize(obj):
+            if type(obj) is list:
+                return [serialize(i) for i in obj]
+            elif isinstance(obj, (str, int, float, bool)) or obj is None:
+                return obj
+            elif isinstance(obj, dict):
+                obj = obj.copy()
+                for key in obj:
+                    obj[key] = serialize(obj[key])
+                return obj
+            else:
+                return json_serial(obj)
+    
         session = self.toolkit_config.session
 
         toolkit = session.query(Toolkit).filter(Toolkit.id == self.toolkit_config.toolkit_id).first()
@@ -46,6 +80,4 @@ class ListAgentTool(BaseTool):
         project = session.query(Project).filter(Project.organisation_id == organisation.id).first()
         agents = session.query(Agent).filter(Agent.project_id == project.id).all()
 
-        agent_list = [object_as_dict(agent) for agent in agents]
-
-        return json.dumps(agent_list)
+        json.dumps(serialize(agents), default=json_serial)
